34b744491e8bd82910f562460e6e2fb136ecfadd
srdubya <stevewy@gmail.com>
2020-01-13 07:30:49 -0700

    Optimizing imports.

--- src/test/java/com/swiley/practice/daily/LongestSequenceTest.java
+++ src/test/java/com/swiley/practice/daily/LongestSequenceTest.java
@@ -3,9 +3,6 @@ package com.swiley.practice.daily;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class LongestSequenceTest {
 
     @Test
